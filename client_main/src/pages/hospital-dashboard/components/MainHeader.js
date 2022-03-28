cbaa5d33f07f2bdb064e42808d5f2ee7d94a487e
Okpainmo <okpainmoandrew@gmail.com>
2022-03-28 08:15:34 +0100

    more progress: cleaned up hospitals dashboard main header, building hospitals dashboard home

--- client_main/src/pages/hospital-dashboard/components/MainHeader.js
+++ client_main/src/pages/hospital-dashboard/components/MainHeader.js
@@ -2,32 +2,50 @@ import userImage from "../../../assets/images/avatar/avatar-2.png";
 
 const MainHeader = () => {
   return (
-    <header className="dashboard-main-header">
+    <header className="dashboard-main-header d-flex justify-content-between">
       <form className="d-flex flex-row dashboard-form">
         <input
           type="search"
           placeholder="Search"
-          className="dashboard-form-input form-control"
+          className="dashboard-form-input form-control position-relative rounded-pill"
         />
         <i className="bi bi-search"></i>
       </form>
 
-      <div className="dashboard-user-details">
-        <div className="dashboard-user">
+      <div className="dashboard-user-details d-flex justify-content-between ">
+        <div className="dashboard-user d-flex">
           <img src={userImage} alt="user" />
-          <div className="user">
+          <div className="user d-flex align-items-center">
             <p>Maitama General Hospital</p>
             <i className="bi bi-chevron-down"></i>
           </div>
         </div>
         <button title="Mail">
-          <i className="bi bi-envelope"></i>
+          <i className="bi bi-envelope position-relative">
+            <span
+              style={{
+                transform: "translate(-50%, 3px)",
+                padding: "5px",
+              }}
+              className="position-absolute top-0 rounded-circle bg-danger">
+              <span class="visually-hidden">unread messages</span>
+            </span>
+          </i>
         </button>
         <button title="Notification">
-          <i className="bi bi-bell"></i>
+          <i className="bi bi-bell position-relative">
+            <span
+              style={{
+                transform: "translate(-115%, 3px)",
+                padding: "5px",
+              }}
+              className="position-absolute top-0 rounded-circle bg-danger">
+              <span class="visually-hidden">unread messages</span>
+            </span>
+          </i>
         </button>
         <button title="Setting">
-          <i className="bi bi-gear"></i>
+          <i className="bi bi-gear-fill" style={{ color: "black" }}></i>
         </button>
       </div>
     </header>
--- client_main/src/pages/hospital-dashboard/index.js
+++ client_main/src/pages/hospital-dashboard/index.js
@@ -1,10 +1,59 @@
 import "../../assets/styles/css/hospital-dashboard.css";
 import HospitalDashboardContainer from "./components/HospitalDashboardContainer";
+import avatar from "../../assets/images/avatar/avatar-2.png";
 
 const HospitalDashboard = () => {
   return (
     <HospitalDashboardContainer>
-      <h1>Welcome to your dashboard</h1>
+      <main className="hospitals-dashboard-home-wrapper d-flex justify-content-center">
+        <section className="main-section">
+          <section className="top-rated p-4">
+            <h5 className="fw-bold">Top Rated</h5>
+            <div className="top-rated-array d-flex justify-content-between pt-3">
+              <div className="top-rated-array-item d-flex align-items-center">
+                <div className="avatar-area position-relative align-items-center d-flex flex-column justify-content-center">
+                  <img
+                    src={avatar}
+                    className="specialist-avatar rounded-circle"
+                    alt="specialist-avatar"
+                  />
+                </div>
+                <div className="details-area position-relative d-flex flex-column ps-3 py-3 pe-2">
+                  <p className="fw-bold">Dr Joshua Anthony</p>
+                  <span>General Medicine</span>
+                </div>
+              </div>
+              <div className="top-rated-array-item d-flex align-items-center">
+                <div className="avatar-area position-relative align-items-center d-flex flex-column justify-content-center">
+                  <img
+                    src={avatar}
+                    className="specialist-avatar rounded-circle"
+                    alt="specialist-avatar"
+                  />
+                </div>
+                <div className="details-area position-relative flex-column d-flex ps-3 py-3 pe-2">
+                  <p className="fw-bold">Dr Ray Matthews</p>
+                  <span>General Medicine</span>
+                </div>
+              </div>
+              <div className="top-rated-array-item d-flex align-items-center">
+                <div className="avatar-area position-relative align-items-center d-flex justify-content-center">
+                  <img
+                    src={avatar}
+                    className="specialist-avatar rounded-circle"
+                    alt="specialist-avatar"
+                  />
+                </div>
+                <div className="details-area position-relative flex-column d-flex ps-3 py-3 pe-2">
+                  <p className="fw-bold">Dr Esther Fish</p>
+                  <span>General Medicine</span>
+                </div>
+              </div>
+            </div>
+          </section>
+        </section>
+        <aside></aside>
+      </main>
     </HospitalDashboardContainer>
   );
 };
