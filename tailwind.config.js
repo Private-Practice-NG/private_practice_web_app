cd01b5499f9d220b9847a8a02fc2e406d1957599
Okpainmo <okpainmoandrew@gmail.com>
2024-04-25 09:07:36 +0100

    chore: added tailwind css to project

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './src/pages/policy-page/**/*.{js,ts,jsx,tsx}',
+    './src/pages/terms-page/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+    screens: {
+      xsm: '450px',
+      // => @media (min-width: 450px) { ... }
+
+      sm: '640px',
+      // => @media (min-width: 640px) { ... }
+
+      md: '768px',
+      // => @media (min-width: 770px) { ... }
+
+      lg: '1150px',
+      // => @media (min-width: 1150px) { ... }
+
+      xl: '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+  plugins: [],
+};
