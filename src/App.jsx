85fa6bc2d9572ee5643baab56652c8537426d2c0
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-06-09 08:33:53 +0100

    refactor: clean-up

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// import { BrowserRouter } from 'react-router-dom';
-// import HospitalsDashboardRouteCenter from './routing-center/HospitalsDashboardRouteCenter';
-// import RegistrationSectionRouteCenter from './routing-center/RegistrationSectionRouteCenter';
-// import SpecialistDashboardRouteCenter from './routing-center/SpecialistDashboardRouteCenter';
-// import Unclassified_RouteCenter from './routing-center/Unclassified_RouteCenter';
-// import ScrollHandler from './components/ScrollHandler';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <ScrollHandler>
-//         <RegistrationSectionRouteCenter />
-//         <HospitalsDashboardRouteCenter />
-//         <SpecialistDashboardRouteCenter />
-//         <Unclassified_RouteCenter />
-//       </ScrollHandler>
-//       {/* <PageNotFound /> */}
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
-
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HospitalsDashboardRouteCenter from './routing-center/HospitalsDashboardRouteCenter';
 import RegistrationSectionRouteCenter from './routing-center/RegistrationSectionRouteCenter';
