25d184d1deef1fe48d7f91e2b6c8fecb681abba7
ikechukwu powel <ikechukwupowei21@gmail.com>
2024-06-09 08:09:13 +0100

    refactor: updated the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,27 @@
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+// import HospitalsDashboardRouteCenter from './routing-center/HospitalsDashboardRouteCenter';
+// import RegistrationSectionRouteCenter from './routing-center/RegistrationSectionRouteCenter';
+// import SpecialistDashboardRouteCenter from './routing-center/SpecialistDashboardRouteCenter';
+// import Unclassified_RouteCenter from './routing-center/Unclassified_RouteCenter';
+// import ScrollHandler from './components/ScrollHandler';
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <ScrollHandler>
+//         <RegistrationSectionRouteCenter />
+//         <HospitalsDashboardRouteCenter />
+//         <SpecialistDashboardRouteCenter />
+//         <Unclassified_RouteCenter />
+//       </ScrollHandler>
+//       {/* <PageNotFound /> */}
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HospitalsDashboardRouteCenter from './routing-center/HospitalsDashboardRouteCenter';
 import RegistrationSectionRouteCenter from './routing-center/RegistrationSectionRouteCenter';
 import SpecialistDashboardRouteCenter from './routing-center/SpecialistDashboardRouteCenter';
@@ -9,10 +32,12 @@ function App() {
   return (
     <BrowserRouter>
       <ScrollHandler>
-        <RegistrationSectionRouteCenter />
-        <HospitalsDashboardRouteCenter />
-        <SpecialistDashboardRouteCenter />
-        <Unclassified_RouteCenter />
+        <Routes>
+          <Route path="/*" element={<RegistrationSectionRouteCenter />} />
+          <Route path="/hospital/*" element={<HospitalsDashboardRouteCenter />} />
+          <Route path="/specialist/*" element={<SpecialistDashboardRouteCenter />} />
+          <Route path="/unclassified/*" element={<Unclassified_RouteCenter />} />
+        </Routes>
       </ScrollHandler>
       {/* <PageNotFound /> */}
     </BrowserRouter>
